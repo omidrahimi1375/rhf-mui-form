b16c478e19a792c9932cd2d3c10fff8c806ada67
Nik <Nik96.ge@gmail.com>
2024-10-12 17:10:23 +0330

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [dts({ rollupTypes: true }), react()],
+
+  base: "./",
+
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "muiForm",
+      formats: ["es", "cjs", "umd", "iife"],
+      // the proper extensions will be added
+      // fileName: "index",
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
+  },
+  publicDir: false
+});
