58ea00d8b28c4f6ead06f693fb40d2ca5897875c
Nik <Nik96.ge@gmail.com>
2024-10-13 17:15:56 +0330

    Select with dynamic value

--- src/components/forms/RHFSelect.tsx
+++ src/components/forms/RHFSelect.tsx
@@ -3,10 +3,14 @@ import { Controller, useFormContext } from "react-hook-form";
 import type { SelectProps } from "@mui/material";
 import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from "@mui/material";
 import type { ReactElement } from "react";
+import { useMemo } from "react";
+import objectHash from "object-hash";
+
+type NotUndefined = object | string | number | boolean | null | NotUndefined[];
 
 interface OptionItem {
   label: string;
-  value: string;
+  value: NotUndefined;
   disabled?: boolean;
 }
 
@@ -19,23 +23,48 @@ type Props<T extends FieldValues> = Omit<SelectProps, "name"> & {
 export default function RHFSelect<T extends FieldValues>({ name, options, control, ...props }: Props<T>): ReactElement {
   const formContext = useFormContext<T>();
 
+  const innerOptions = useMemo(() => {
+    const result: Record<string, OptionItem> = {};
+
+    for (const o of options) {
+      result[objectHash(o.value)] = o;
+    }
+
+    return result;
+  }, [options]);
+
   return (
     <Controller
       name={name}
       control={control ?? formContext.control}
-      render={({ field: { value, ...field }, fieldState: { error } }) => (
-        <FormControl fullWidth={true} disabled={props.disabled}>
-          <InputLabel>{props.label}</InputLabel>
-          <Select {...props} error={error !== undefined} value={value ?? ""} {...field}>
-            {options.map((option) => (
-              <MenuItem value={option.value} key={option.value} disabled={option.disabled}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </Select>
-          {props.disabled !== true ? <FormHelperText>{error?.message}</FormHelperText> : null}
-        </FormControl>
-      )}
+      render={({ field: { value, onChange, ...field }, fieldState: { error } }) => {
+        // TODO: we can move it up somehow to not run in every select onChange.
+        if (!(objectHash(value) in innerOptions)) {
+          throw new Error("This value is not present in the options: ", value);
+        }
+
+        return (
+          <FormControl fullWidth={true} disabled={props.disabled} error={error !== undefined}>
+            <InputLabel>{props.label}</InputLabel>
+            <Select
+              {...props}
+              error={error !== undefined}
+              value={value === undefined ? "" : objectHash(value)}
+              {...field}
+              onChange={(event) => {
+                onChange(innerOptions[event.target.value as string].value);
+              }}
+            >
+              {Object.entries(innerOptions).map(([hash, option]) => (
+                <MenuItem value={hash} key={hash} disabled={option.disabled}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>{error?.message ?? " "}</FormHelperText>
+          </FormControl>
+        );
+      }}
     />
   );
 }
