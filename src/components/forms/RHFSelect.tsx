e60d1e3a8d64110a80951738a309da07853b383e
Nik <Nik96.ge@gmail.com>
2024-10-14 14:54:23 +0330

    Complete SelectBox

--- src/components/forms/RHFSelect.tsx
+++ src/components/forms/RHFSelect.tsx
@@ -1,29 +1,103 @@
 import type { Control, FieldValues, Path } from "react-hook-form";
+import { useWatch } from "react-hook-form";
 import { Controller, useFormContext } from "react-hook-form";
 import type { SelectProps } from "@mui/material";
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from "@mui/material";
+import { Checkbox, MenuItem } from "@mui/material";
+import { FormControl, FormHelperText, InputLabel, Select } from "@mui/material";
 import type { ReactElement } from "react";
 import { useMemo } from "react";
 import objectHash from "object-hash";
+import SelectRenderValue from "./components/SelectRenderValue.tsx";
 
 type NotUndefined = object | string | number | boolean | null | NotUndefined[];
 
 interface OptionItem {
+  /** The label displayed for the option */
   label: string;
+  /** The value of the option, which can be any type */
   value: NotUndefined;
+  /** Optional flag to disable the option */
   disabled?: boolean;
 }
 
 type Props<T extends FieldValues> = Omit<SelectProps, "name"> & {
+  /** The name of the field in the form state */
   readonly name: Path<T>;
+  /** An array of option items to be displayed in the select dropdown */
   readonly options: OptionItem[];
+  /** The control object from React Hook Form, optional if useFormContext is used */
   readonly control?: Control<T>;
+  /** The direction of the text input, either left-to-right (ltr) or right-to-left (rtl) */
+  readonly inputDir?: "ltr" | "rtl";
+  /** The maximum height of the select input */
+  readonly maxHeight?: number;
+  /** The maximum height of the dropdown menu */
+  readonly dropDownMaxHeight?: number;
 };
 
-export default function RHFSelect<T extends FieldValues>({ name, options, control, ...props }: Props<T>): ReactElement {
+/**
+ * `RHFSelect` is a wrapper around MIUI's `Select` component that integrates with React Hook Form.
+ * It supports both single and multiple selections and handles validation and error messages.
+ *
+ * - This component automatically handles the form control via React Hook Form and provides seamless integration.
+ * - The `options` prop allows for dynamic option generation, including support for disabling options.
+ *
+ * @template T - A generic type for the form's field values, extending `FieldValues`.
+ *
+ * @param {Path<T>} name - The name of the field in the form state.
+ * @param {OptionItem[]} options - An array of option items to be displayed in the select dropdown.
+ * @param {Control<T>} [control] - The React Hook Form control object. If not provided, the form context will be used.
+ * @param {"ltr" | "rtl"} [inputDir] - The direction of the text input, either left-to-right or right-to-left.
+ * @param {number} [maxHeight] - The maximum height of the select input.
+ * @param {number} [dropDownMaxHeight] - The maximum height of the dropdown menu.
+ * @param {SelectProps} props - Additional props passed to the underlying MUI `Select`.
+ *
+ * @returns {ReactElement} A controlled select component integrated with React Hook Form.
+ *
+ * @example
+ * ```tsx
+ * <RHFSelect
+ *   name="favoriteFruits"
+ *   options={[
+ *     { label: "Apple", value: "apple" },
+ *     { label: "Banana", value: "banana" },
+ *     { label: "Cherry", value: "cherry", disabled: true },
+ *   ]}
+ *   control={control} // Optional, if useFormContext is not used
+ * />
+ * ```
+ *
+ * @example
+ * ```tsx
+ * <RHFSelect
+ *   name="selectedColors"
+ *   options={[
+ *     { label: "Red", value: "red" },
+ *     { label: "Green", value: "green" },
+ *     { label: "Blue", value: "blue" },
+ *   ]}
+ *   multiple
+ *   inputDir="ltr"
+ * />
+ * ```
+ */
+export default function RHFSelect<T extends FieldValues>({
+  name,
+  options,
+  control,
+  inputDir,
+  maxHeight,
+  dropDownMaxHeight,
+  ...props
+}: Props<T>): ReactElement {
   const formContext = useFormContext<T>();
+  const rhfValue: string | string[] | undefined = useWatch({ control: control ?? formContext.control, name });
 
-  const innerOptions = useMemo(() => {
+  const isMultiple = useMemo(() => {
+    return props.multiple === true;
+  }, [props.multiple]);
+
+  const hashedOptions = useMemo(() => {
     const result: Record<string, OptionItem> = {};
 
     for (const o of options) {
@@ -33,33 +107,78 @@ export default function RHFSelect<T extends FieldValues>({ name, options, contro
     return result;
   }, [options]);
 
+  const hashedValue: string | string[] = useMemo(() => {
+    if (rhfValue === undefined) {
+      return isMultiple ? [] : "";
+    }
+
+    return isMultiple ? (rhfValue as string[]).map((s) => objectHash(s)) : objectHash(rhfValue);
+  }, [isMultiple, rhfValue]);
+
   return (
     <Controller
       name={name}
       control={control ?? formContext.control}
-      render={({ field: { value, onChange, ...field }, fieldState: { error } }) => {
-        // TODO: we can move it up somehow to not run in every select onChange.
-        if (!(objectHash(value) in innerOptions)) {
-          throw new Error("This value is not present in the options: ", value);
-        }
-
+      render={({ field: { value: _, onChange, ...field }, fieldState: { error } }) => {
         return (
           <FormControl fullWidth={true} disabled={props.disabled} error={error !== undefined}>
             <InputLabel>{props.label}</InputLabel>
             <Select
               {...props}
               error={error !== undefined}
-              value={value === undefined ? "" : objectHash(value)}
+              value={hashedValue}
               {...field}
               onChange={(event) => {
-                onChange(innerOptions[event.target.value as string].value);
+                onChange(
+                  isMultiple
+                    ? (event.target.value as string[]).map((v) => hashedOptions[v].value)
+                    : hashedOptions[event.target.value as string].value
+                );
               }}
+              MenuProps={{
+                ...props.MenuProps,
+                slotProps: {
+                  ...props.MenuProps?.slotProps,
+                  paper: {
+                    ...props.MenuProps?.slotProps?.paper,
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    style: {
+                      maxHeight: dropDownMaxHeight,
+                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                      // @ts-expect-error
+                      ...props.MenuProps?.slotProps?.paper?.style
+                    }
+                  }
+                }
+              }}
+              renderValue={
+                // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+                props.renderValue !== undefined
+                  ? props.renderValue
+                  : isMultiple
+                    ? (s) => (
+                        <SelectRenderValue
+                          options={hashedOptions}
+                          selected={s as string | string[]}
+                          maxHeight={maxHeight}
+                          inputDir={inputDir}
+                        />
+                      )
+                    : undefined
+              }
             >
-              {Object.entries(innerOptions).map(([hash, option]) => (
-                <MenuItem value={hash} key={hash} disabled={option.disabled}>
-                  {option.label}
-                </MenuItem>
-              ))}
+              {isMultiple
+                ? Object.entries(hashedOptions).map(([hash, option]) => (
+                    <MenuItem value={hash} key={hash} disabled={option.disabled} dir={inputDir}>
+                      <Checkbox checked={hashedValue.includes(hash)} />
+                      {option.label}
+                    </MenuItem>
+                  ))
+                : Object.entries(hashedOptions).map(([hash, option]) => (
+                    <MenuItem value={hash} key={hash} disabled={option.disabled} dir={inputDir}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
             </Select>
             <FormHelperText>{error?.message ?? " "}</FormHelperText>
           </FormControl>
---
+++ src/components/forms/components/SelectRenderValue.tsx
@@ -0,0 +1,46 @@
+import type { ReactElement } from "react";
+import { memo } from "react";
+import type { SelectOption } from "../types.ts";
+import { Chip, Stack } from "@mui/material";
+
+interface Props {
+  readonly options: Record<string, SelectOption>;
+  readonly selected: string | string[];
+  readonly maxHeight: number | undefined;
+  readonly inputDir: "ltr" | "rtl" | undefined;
+}
+
+interface ChipsProps {
+  readonly options: Props["options"];
+  readonly selected: Props["selected"];
+}
+
+function Chips({ options, selected }: ChipsProps): ReactElement | ReactElement[] {
+  if (typeof selected === "string") {
+    return <Chip key={selected} label={options[selected].label} />;
+  }
+
+  return selected.map((s) => {
+    return <Chip key={s} label={options[s].label} />;
+  });
+}
+
+function SelectRenderValue({ options, selected, maxHeight, inputDir }: Props): ReactElement {
+  if (maxHeight === undefined) {
+    return (
+      <Stack direction="row" flexWrap="wrap" gap={1}>
+        <Chips options={options} selected={selected} />
+      </Stack>
+    );
+  }
+
+  return (
+    <Stack dir="ltr" maxHeight={maxHeight} sx={{ overflowY: "auto" }}>
+      <Stack dir={inputDir} direction="row" flexWrap="wrap" gap={1}>
+        <Chips options={options} selected={selected} />
+      </Stack>
+    </Stack>
+  );
+}
+
+export default memo(SelectRenderValue);
